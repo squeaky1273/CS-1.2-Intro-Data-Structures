e5b45df90f43e3449dace3e7104675a309c1ef74
squeaky1273 <pjriddell22@gmail.com>
2019-10-23 15:57:45 -0700

    fixed rearrnage.py

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,19 +1,19 @@
-import random
 from random import choice
 import sys
 
 def rearrange(words):
     """Rearrange words that are provided"""
     word_list = []
-    random.shuffle(words)
-    while words in word_list:
+    count = 0
+    while count < len(words):
         new_word = choice(words)
-        if new_word != word_list:
+        if new_word not in word_list:
             word_list.append(new_word)
-    return word_list
+            count += 1
+    return (" ".join(word_list))
 
 if __name__ == '__main__':
     words = sys.argv[1:]
     word_list = rearrange(words)
-    print(words)
+    print(word_list)
     
