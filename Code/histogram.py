4d7890c7a9aedd37a991a2389ac6992ac0a0ba08
squeaky1273 <pjriddell22@gmail.com>
2019-10-26 20:43:40 -0700

    added separate functions

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,22 +1,43 @@
 import sys
 from random import choice
 
-def histogram(file_name):
-    """Writes histograms of reoccuring words"""
-    #list of lists
+def list_of_lists(word_list):
     histogram = []
-    for word in file_name:
-    
+    for words in word_list:
+        if words in histogram:
+            histogram[words] += 1
+        else:
+            histogram[words] = 1
+
+def dictionary(word_list):
     #dictionary
     histogram = {}
-    for word in file_name:
-    
+    for words in word_list:
+        if words in histogram:
+            histogram[words] += 1
+        else:
+            histogram[words] = 1
+
+def tuples(word_list):
     #list of tuples
     histogram = [] 
-    for word in file_name:
-    
+    for words in word_list:
+        if words in histogram:
+            histogram[words] += 1
+        else:
+            histogram[words] = 1
+
+#def list_of_counts():
     #list of counts
     #histogram = []
+
+def histogram(file_name):
+    """Writes histograms of reoccuring words"""
+    word_list = file_name
+    #histogram = list_of_lists(word_list)
+    #histogram = tuples(word_list)
+    histogram = dictionary(word_list)
+    return histogram
 
 def unique_words(histogram):
     """Takes the histogram arguments"""
@@ -31,7 +52,8 @@ def frequency(words, histogram):
     return histogram[words]
 
 if __name__ == "__main__":
-    #f = open("text.txt", "r")
-    #content = f.read()
-    #histogram(content)
+    histogram = histogram(sys.argv[1])
+    f = open("text.txt", "r")
+    content = f.read()
+    histogram(content)
     pass
