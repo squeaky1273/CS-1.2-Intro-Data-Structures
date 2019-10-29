4bb50e4b69849e2feb33e1248bdc35f3c2eb6bca
squeaky1273 <pjriddell22@gmail.com>
2019-10-29 14:37:23 -0700

    added comments to explain function

--- Code/sample.py
+++ Code/sample.py
@@ -5,9 +5,9 @@ import random
 def sample_by_frequency(histogram):
     """Returns a single word at random 
     from the provided words/text"""
-    word_list = open("text.txt", "r")
-    words = list(histogram)
-    word_index = random.randint(0, len(words) - 1)
+    word_list = open("text.txt", "r") #Open text file
+    words = list(histogram) #Get the list of lists from the histogram
+    word_index = random.randint(0, len(words) - 1) #Randomly choose a word from the list
     word_list = words[word_index]
     return word_list
     
