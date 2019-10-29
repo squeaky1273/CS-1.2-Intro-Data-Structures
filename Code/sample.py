2ef1cc33cb5d88a2bb14aaf2f8eae4df00cb8a30
squeaky1273 <pjriddell22@gmail.com>
2019-10-29 13:16:50 -0700

    added some code to sample.py

--- Code/sample.py
+++ Code/sample.py
@@ -5,8 +5,12 @@ import random
 def sample_by_frequency(histogram):
     """Returns a single word at random 
     from the provided words/text"""
-
-
+    word_list = ""
+    words = list(histogram)
+    word_index = random.randint(0, len(words) - 1)
+    word_list = words[word_index]
+    return word_list
+    
 if __name__ == "__main__":
     histogram = histogram.histogram(sys.argv[1])
     print(sample_by_frequency(histogram))
