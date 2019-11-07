1d40ac1a7a9060ea97faf3a8e4ffe6170c1b263d
squeaky1273 <pjriddell22@gmail.com>
2019-11-07 14:26:07 -0800

    added fisher yates

---
+++ Code/markov.py
@@ -0,0 +1,19 @@
+from dictogram import Dictogram
+from listogram import Listogram
+
+class Markov_chain():
+    def markov_chain():
+        markov = {}
+        for _ in range(len(word_list)):
+            if:
+            if:
+        pass
+
+    def markov_create():
+        pass
+
+    def run():
+        pass
+
+if __name__ ==  '__main__':
+    #run()
--- Code/rearrange.py
+++ Code/rearrange.py
@@ -18,8 +18,16 @@ def reverse(words):
     output = inputWord[::-1]
     return output
 
+def Fisher_Yates(words):
+    """Shuffling and pulling a word out of the list"""
+    for i in range(len(words)):
+        random_word = random.randint(0, i + 1)
+        words[i], words[random_word] = words[random_word], words[i]  
+    print (str(words))
+
 if __name__ == '__main__':
     words = sys.argv[1:]
     #word_list = rearrange(words)
     #print(word_list)
-    print(reverse(words))
+    #print(reverse(words))
+    print(Fisher_Yates(words))
