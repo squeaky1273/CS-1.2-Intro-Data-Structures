90fae9ff5b1995f313fafba0d98697d22ec61e00
squeaky1273 <pjriddell22@gmail.com>
2019-11-08 17:11:12 -0800

    added more code to markov chain

--- Code/markov.py
+++ Code/markov.py
@@ -1,21 +1,40 @@
 from dictogram import Dictogram
 #from listogram import Listogram
-
+""""
 class Markov_chain():
-    def __init__(self):
+    def __init__(self, words=None):
+        self.words = list()
+            if words = None:
+                pass
+"""
+f = open(text.txt)
+text = f.split
 
-    def markov_chain():
-        markov = {}
-        for _ in range(len(word_list)):
-            if:
-            if:
-        pass
+def markov_chain(text):
+    for _ in range(len(text)-1):
+     (text[_], text[_+1])
 
-    def markov_create():
-        pass
+    pairs = markov_chain(text)
 
-    def run():
-        pass
+    markov = {}
+    for word_1, word_2 in pairs:
+        if word_1[_] in markov.keys:
+            markov[word_1.append(word_2)
+        else:
+            markov[word_1] = [word_2]
+        return markov
+
+def random_word():
+    pass
+
+def random_walk(word):
+    sentence = []
+    sentence.append(word)
+    for _ in range(word):
+        sentence.append(word)
+    
+    return sentence
+    pass
 
 if __name__ ==  '__main__':
-    #run()
+    random_walk(markov_chain(text))
